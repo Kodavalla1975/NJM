17ed5dab75a4914e895741fd0f366635416edbc1
qanagaraju <qatrainer4u@gmail.com>
2023-09-30 17:36:48 +0530

    first commit

---
+++ src/main/java/NJM_Digtal/App.java
@@ -0,0 +1,13 @@
+package NJM_Digtal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
